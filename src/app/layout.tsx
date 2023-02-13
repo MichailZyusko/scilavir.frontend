60e7091509f820d4d921f92bcabbb4c7abe19b6f
MichailZyusko <michail.zyusko@gmail.com>
2023-02-13 17:34:19 +0300

    chore: init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren } from 'react';
+import './globals.css';
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  );
+}
