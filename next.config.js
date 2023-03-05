c85662fc9d74a524d98f025ebefe2370fca3c590
MichailZyusko <michail.zyusko@gmail.com>
2023-03-05 15:19:03 +0300

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,14 +6,6 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_BASE_API_URL: process.env.NEXT_PUBLIC_BASE_API_URL
   },
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://scilavir-backend.vercel.app/:path*'
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
