b79c580ab767f952bd5acb3c96c53d118cfcfc82
MichailZyusko <michail.zyusko@gmail.com>
2023-03-05 12:16:16 +0300

    fix: `CORS` issue resolving

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   },
   env: {
     NEXT_PUBLIC_BASE_API_URL: process.env.NEXT_PUBLIC_BASE_API_URL
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://scilavir-backend.vercel.app/:path*'
+      }
+    ]
   }
 }
 
