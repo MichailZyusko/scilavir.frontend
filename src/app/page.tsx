e7caf5301a88c3a5ad1d2b8dbdd4c9f7d2ac7e8d
MichailZyusko <michail.zyusko@gmail.com>
2023-04-23 15:27:56 +0300

    update src/api/axios/index.ts, page.tsx and routes.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,23 @@ import { Banner } from '@/ui-kit/components/overview/banner';
 import { PopularCategories } from '@/ui-kit/components/overview/popular-categories';
 import { ProsList } from '@/ui-kit/components/overview/pros-list';
 
-export default async function Home() {
-  // ! TODO: Add handling access_token & refresh token for supabase
+type TProps = {
+  searchParams?: {
+    access_token: string;
+    refresh_token: string;
+  };
+};
+
+export default async function Home({ searchParams }: TProps) {
+  const {
+    access_token: accessToken,
+    refresh_token: refreshToken,
+  } = searchParams || {};
+
+  if (accessToken && refreshToken) {
+    localStorage.setItem('a-token', accessToken);
+    localStorage.setItem('r-token', refreshToken);
+  }
 
   return (
     <main className="px-44">
