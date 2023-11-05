1a26fccca43b500e9a029457d4afb7b5ce8ca2fd
MichailZyusko <michail.zyusko@gmail.com>
2023-11-05 12:33:21 +0300

    fix: improve UI/UX

--- src/app/categories/[id]/page.tsx
+++ src/app/categories/[id]/page.tsx
@@ -11,9 +11,16 @@ import { SortStrategy } from '@/enums';
 import { useClerkToken } from '@/context/auth';
 import { Loader } from '@/ui-kit/spinners';
 
+type TCategory = {
+  id: string;
+  name: string;
+  description: string;
+  subCategories: TCategory[];
+};
+
 type TState = {
   products: TProduct[];
-  subCategories: [];
+  category: TCategory | null;
   sort: SortStrategy;
   isLoading: boolean;
 };
@@ -28,7 +35,7 @@ export default function CategoryPage({ params: { id: categoryId = '' } }: TProps
   const { updateClerkToken } = useClerkToken();
   const [state, setState] = useState<TState>({
     products: [],
-    subCategories: [],
+    category: null,
     sort: SortStrategy.PRICE_ASC,
     isLoading: true,
   });
@@ -39,20 +46,20 @@ export default function CategoryPage({ params: { id: categoryId = '' } }: TProps
     (async () => {
       await updateClerkToken();
 
-      const [{ data: subCategories }, { data: products }] = await Promise.all([
-        axios.get<[]>(`/categories/${categoryId}`),
-        axios.get<TProduct[]>('/products', {
-          params: {
-            categoryIds: [categoryId],
-            sort,
-          },
-        }),
-      ]);
+      const { data: { category } } = await axios.get<{ category: TCategory }>(`/categories/${categoryId}`);
+      const { data: products } = await axios.get<TProduct[]>('/products', {
+        params: {
+          categoryIds: category.subCategories.length
+            ? category.subCategories.map(({ id }) => id)
+            : [categoryId],
+          sort,
+        },
+      });
 
       setState({
         ...state,
         products,
-        subCategories,
+        category,
         isLoading: false,
       });
     })();
@@ -61,10 +68,11 @@ export default function CategoryPage({ params: { id: categoryId = '' } }: TProps
   if (state.isLoading) {
     return <Loader />;
   }
+
   return (
     <main className="px-44">
-      <SubCategoryList categories={state.subCategories} />
-      <h1 className="w-full text-4xl text-center font-semibold mb-5">Каталог</h1>
+      <SubCategoryList categories={state?.category?.subCategories} />
+      <h1 className="w-full text-4xl text-center font-semibold mb-5">{state.category?.name}</h1>
       <div className="flex justify-between mb-2.5">
         <Dropdown
           label="Сортировать"
--- src/ui-kit/components/products/sub-categorie.tsx
+++ src/ui-kit/components/products/sub-categorie.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link';
 
 type TSubCategory = {
-  id: number;
+  id: string;
   name: string;
 };
 
 function SubCategory({ name, id }: TSubCategory) {
   return (
-    <Link className="flex flex-col w-min items-center justify-center" href={`/category/${id}`}>
+    <Link className="flex flex-col w-min items-center justify-center" href={`/categories/${id}`}>
       <div className="w-44 h-44 bg-gray" />
       <h2 className="text-center font-semibold">{name}</h2>
     </Link>
@@ -15,9 +15,13 @@ function SubCategory({ name, id }: TSubCategory) {
 }
 
 type TProps = {
-  categories: TSubCategory[]
+  categories?: TSubCategory[]
 };
 export function SubCategoryList({ categories }: TProps) {
+  if (!(categories && categories.length)) {
+    return null;
+  }
+
   return (
     <div className="flex justify-between mb-16">
       {categories.map(({ name, id }) => (
