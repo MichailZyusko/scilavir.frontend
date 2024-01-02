8ad56ec7ebf3c393c79b1495d42b5d22fbfc9ef5
MichailZyusko <michail.zyusko@gmail.com>
2024-01-02 15:24:51 +0300

    fix: pagination color mismatch

--- src/ui-kit/components/paginator/index.tsx
+++ src/ui-kit/components/paginator/index.tsx
@@ -9,7 +9,7 @@ const paginationTheme: FlowbitePaginationTheme = {
     },
   },
   pages: {
-    base: 'xs:mt-0 mt-2 inline-flex items-center -space-x-px',
+    base: 'xs:mt-0 mt-2 inline-flex items-center space-x-px',
     showIcon: 'inline-flex',
     previous: {
       base: 'ml-0 bg-white py-2 px-3 leading-tight text-gray-500 enabled:hover:bg-gray-100 enabled:hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 enabled:dark:hover:bg-gray-700 enabled:dark:hover:text-white',
@@ -20,8 +20,8 @@ const paginationTheme: FlowbitePaginationTheme = {
       icon: 'h-5 w-5',
     },
     selector: {
-      base: 'w-10 border-gray-300 bg-white py-2 px-2 leading-tight text-gray-500 enabled:hover:bg-gray-100 enabled:hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 enabled:dark:hover:bg-gray-700 enabled:dark:hover:text-white',
-      active: 'bg-yellow-400 opacity-50 rounded-full text-black hover:bg-cyan-100 hover:text-cyan-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white',
+      base: 'w-10 border-gray-300 bg-white py-2 px-2 text-gray-500 enabled:hover:bg-gray-100 enabled:hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 enabled:dark:hover:bg-gray-700 enabled:dark:hover:text-white',
+      active: 'bg-yellow-400 bg-opacity-50 rounded-full text-black hover:bg-cyan-100 hover:text-cyan-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white',
       disabled: 'opacity-50 cursor-normal',
     },
   },
@@ -32,7 +32,11 @@ type TProps = {
   totalPages: number;
   onPageChange: (page: number) => void;
 };
-export function Paginator({ currentPage, totalPages, onPageChange }:TProps) {
+export function Paginator({ currentPage, totalPages, onPageChange }: TProps) {
+  if (!totalPages) {
+    return null;
+  }
+
   return (
     <Pagination
       theme={paginationTheme}
